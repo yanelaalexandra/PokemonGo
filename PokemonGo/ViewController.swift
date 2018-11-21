45dac1725ee7d84a71d1bbe15c2e76d2fdae4040
Yanela Pachacama Quispe <ypachacamaq@tecsup.edu.pe>
2018-11-21 13:25:22 -0500

    Initial Commit

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by Yanela Pachacama Quispe on 21/11/18.
+//  Copyright © 2018 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
